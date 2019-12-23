8327359776a47a8536d2e4c0ec26832b5b18346f
adithyaov <adi.obilisetty@gmail.com>
2019-12-24 00:59:41 +0530

    Major rewrite of all the modules and tests

--- test/Main.hs
+++ test/Main.hs
@@ -1,46 +1,51 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module Main where
 
 import Control.Monad (filterM)
-import qualified Data.ByteString as BS
-import Streamly.ByteString
-import qualified Streamly.ByteString.Lazy as SL
-import Streamly.FileSystem.File (readArrays)
+import Data.ByteString (ByteString)
+import Data.Word (Word8)
+import Streamly
+import Streamly.Memory.Array (Array)
 import System.Directory
 import System.FilePath ((</>))
+import Streamly.FileSystem.Handle (readChunks)
 import Test.Hspec
 import Test.Hspec.QuickCheck
+import System.IO (openFile, IOMode(ReadMode))
 import Test.QuickCheck.Instances.ByteString
+
+import qualified Data.ByteString as BS
+import qualified Streamly.ByteString as Strict
+import qualified Streamly.ByteString.Lazy as Lazy
+import qualified Streamly.Prelude as S
+
+streamToByteString :: MonadAsync m => SerialT m (Array Word8)-> m ByteString
+streamToByteString stream = S.foldl' (<>) mempty $ S.map Strict.fromArray stream
 
 checkFileContent :: FilePath -> IO ()
 checkFileContent filename = do
   print $ "Checking " <> filename
   bsContent <- BS.readFile filename
-  let arrays = readArrays filename
-  bsStreamly <- toByteString arrays
+  handle <- openFile filename ReadMode
+  bsStreamly <- streamToByteString $ S.unfold readChunks handle
   bsContent `shouldBe` bsStreamly
 
 getHundredFiles :: FilePath -> IO [FilePath]
 getHundredFiles path = do
   files <- listDirectory path
   files2 <- filterM (\x -> doesFileExist $ path </> x) files
-  pure $ take 100 $ map (\x -> path </> x) files2
+  return $ take 100 $ map (\x -> path </> x) files2
 
 main :: IO ()
 main =
   hspec $ do
     describe "array functions" $ do
-      it "arrayToByteString" $ do
-        filenames <- getHundredFiles "/home/sibi"
+      it "Strict fromArray" $ do
+        filenames <- getHundredFiles "/home/creed/"
         mapM_ checkFileContent filenames
-      prop "identity function" $ \bs -> do
-        arr <- fromByteString bs
-        bs2 <- arrayToByteString arr
-        bs `shouldBe` bs2
-      prop "fromBytestring . toByteString == id" $ \bs -> do
-        arr <- fromByteString bs
-        bs2 <- toByteString (return arr)
-        bs `shouldBe` bs2
-      prop "(lazy) fromBytestring . toByteString == id" $ \bs -> do
-        let arr = SL.fromByteString bs
-        bs2 <- SL.toByteString arr
+      prop "Strict Identity" $ \bs ->
+        bs `shouldBe` Strict.fromArray (Strict.toArray bs)
+      prop "Lazy Identity" $ \bs -> do
+        bs2 <- Lazy.fromArray . Lazy.toArray $ bs
         bs `shouldBe` bs2
