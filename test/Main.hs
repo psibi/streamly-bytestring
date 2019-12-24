452ab629be363015ee8de2f5e876d8deef31a0c9
adithyaov <adi.obilisetty@gmail.com>
2019-12-24 15:16:38 +0530

    Make tests portable

--- test/Main.hs
+++ test/Main.hs
@@ -2,17 +2,18 @@
 
 module Main where
 
-import Control.Monad (filterM)
 import Data.ByteString (ByteString)
 import Data.Word (Word8)
+import GHC.IO.Handle (Handle)
 import Streamly
+import Streamly.FileSystem.Handle (readChunks)
 import Streamly.Memory.Array (Array)
 import System.Directory
 import System.FilePath ((</>))
-import Streamly.FileSystem.Handle (readChunks)
+import System.IO (openFile, IOMode(ReadMode))
+import System.IO.Temp (withSystemTempFile)
 import Test.Hspec
 import Test.Hspec.QuickCheck
-import System.IO (openFile, IOMode(ReadMode))
 import Test.QuickCheck.Instances.ByteString
 
 import qualified Data.ByteString as BS
@@ -23,29 +24,22 @@ import qualified Streamly.Prelude as S
 streamToByteString :: MonadAsync m => SerialT m (Array Word8)-> m ByteString
 streamToByteString stream = S.foldl' (<>) mempty $ S.map Strict.fromArray stream
 
-checkFileContent :: FilePath -> IO ()
-checkFileContent filename = do
+checkFileContent :: FilePath -> Handle -> IO ()
+checkFileContent filename handle = do
   print $ "Checking " <> filename
-  bsContent <- BS.readFile filename
+  bsContent <- BS.hGetContents handle
   handle <- openFile filename ReadMode
   bsStreamly <- streamToByteString $ S.unfold readChunks handle
   bsContent `shouldBe` bsStreamly
 
-getHundredFiles :: FilePath -> IO [FilePath]
-getHundredFiles path = do
-  files <- listDirectory path
-  files2 <- filterM (\x -> doesFileExist $ path </> x) files
-  return $ take 100 $ map (\x -> path </> x) files2
-
 main :: IO ()
 main =
-  hspec $ do
-    describe "array functions" $ do
-      it "Strict fromArray" $ do
-        filenames <- getHundredFiles "/home/creed/"
-        mapM_ checkFileContent filenames
+  hspec $
+    describe "Array tests" $ do
+      it "Strict fromArray" $
+        mapM_ (flip withSystemTempFile checkFileContent . show) [1..100] 
       prop "Strict Identity" $ \bs ->
         bs `shouldBe` Strict.fromArray (Strict.toArray bs)
       prop "Lazy Identity" $ \bs -> do
-        bs2 <- Lazy.fromArray . Lazy.toArray $ bs
+        bs2 <- Lazy.fromArrayStream . Lazy.toArrayStream $ bs
         bs `shouldBe` bs2
