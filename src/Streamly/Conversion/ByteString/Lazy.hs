7438f4e1e66d795c38eeca4cbef8432fd1270705
adithyaov <adi.obilisetty@gmail.com>
2019-12-24 15:14:46 +0530

    Modify lazy function names and definitions

--- src/Streamly/Conversion/ByteString/Lazy.hs
+++ src/Streamly/Conversion/ByteString/Lazy.hs
@@ -1,17 +1,18 @@
 module Streamly.Conversion.ByteString.Lazy where
 
-import Data.ByteString.Lazy (ByteString, foldlChunks, fromChunks)
+import Data.ByteString.Lazy.Internal (ByteString(..), foldlChunks)
 import Data.Word (Word8)
 import Streamly.Conversion.ByteString as Strict
 import Streamly.Internal.Memory.Array.Types (Array(..))
 import Streamly
 
 import qualified Streamly.Prelude as S
+import qualified Streamly.Internal.Data.Stream.StreamD as D
 
-fromArray :: Monad m => SerialT m (Array Word8) -> m ByteString
-fromArray = fmap fromChunks . S.toList . S.map Strict.fromArray
+fromArrayStream :: Monad m => SerialT m (Array Word8) -> m ByteString
+fromArrayStream = S.foldl' (flip Chunk) Empty . S.map Strict.fromArray
 
-toArray :: Monad m => ByteString -> SerialT m (Array Word8)
-toArray = foldlChunks stepFunction S.nil
+toArrayStream :: Monad m => ByteString -> SerialT m (Array Word8)
+toArrayStream = D.fromStreamD . foldlChunks stepFunction D.nil
   where
-    stepFunction s b = s <> return (Strict.toArray b) 
+    stepFunction s = flip D.cons s . Strict.toArray 
